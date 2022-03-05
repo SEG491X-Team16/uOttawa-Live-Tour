a0e8b5bef83c4abb6b7e2e979356ce178ba8b7f5
Angelica-P <apayn049@uottawa.ca>
2022-03-05 12:36:13 -0500

    Finishing GPSSingleton, cleaning it up and adding Android LogCat to packages.

--- uOttawa_Live_Tour/Assets/Scripts/GPSConsumer.cs
+++ uOttawa_Live_Tour/Assets/Scripts/GPSConsumer.cs
@@ -8,23 +8,20 @@ public class GPSConsumer : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI locationUpdate;
 
-    GPSSingleton gpsSingleton;
-
     private int i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject gpsObject = GameObject.Find("GPS");
-        gpsSingleton = gpsObject.GetComponent<GPSSingleton>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gpsSingleton.isDataValid())
+        if (GPSSingleton.Instance.isDataValid())
         {
-            locationUpdate.text =  i + ":" + gpsSingleton.getLastUpdate() + " : "+ gpsSingleton.getLatitude() + " : " + gpsSingleton.getLongitude();
+            locationUpdate.text =  i + ":" + GPSSingleton.Instance.getLastUpdate() + " : "+ GPSSingleton.Instance.getLatitude() + " : " + GPSSingleton.Instance.getLongitude();
             i++;
         }
     }
--- uOttawa_Live_Tour/Assets/Scripts/GPSSingleton.cs
+++ uOttawa_Live_Tour/Assets/Scripts/GPSSingleton.cs
@@ -4,42 +4,43 @@ using UnityEngine;
 using UnityEngine.Android;
 using TMPro;
 
-// Adapted from code at: https://nosuchstudio.medium.com/how-to-access-gps-location-in-unity-521f1371a7e3
+/**
+* This class is created once per scene and it provides the location to all assets/scripts that require it.
+* It is added to an Empty. This way all classes will have access to the same GPS coordinates.
+* 
+* Adapted from code at: https://nosuchstudio.medium.com/how-to-access-gps-location-in-unity-521f1371a7e3
+*/
 
 public class GPSSingleton : MonoBehaviour
 {
 
-    [SerializeField] TextMeshProUGUI location;
-
+    //The latitude and Longitude in Decimal Degrees
     private double latitude = 0.0;
     private double longitude =  0.0;
 
+    //The timestamp of the last location update
     private double lastUpdate = -1.0;
 
-    // public static GPSSingleton Instance { get; private set; }
+    //The instance of the class
+    public static GPSSingleton Instance { get; private set; }
 
-    // private void Awake() 
-    // {
-    //     if (Instance != null && Instance != this)
-    //     {
-    //         Destroy(this);
-    //         return;
-    //     }
+    //makes sure no more than one instance of the class exists at once
+    private void Awake() 
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
 
-    //     Instance = this;
-    // }
+        Instance = this;
+    }
 
+    //connect to the devices location services and update the stored variables
     IEnumerator GPSCoroutine() {
 
-        location.text = "start coroutine";
-
         //Persmissions
-        #if UNITY_EDITOR
-            //no permissions in editor
-
-        yield return new WaitWhile(() => !UnityEditor.EditorApplication.isRemoteConnected);
-        yield return new WaitForSecondsRealtime(5f);
-        #elif UNITY_ANDROID
+        #if UNITY_ANDROID
         
         if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
         {
@@ -47,7 +48,6 @@ public class GPSSingleton : MonoBehaviour
         }
 
         if (!UnityEngine.Input.location.isEnabledByUser) {
-            location.text = "location no go";
             Debug.Log("Android Location not enabled");
             yield break;
             //we need to do something here to let user know
@@ -62,8 +62,6 @@ public class GPSSingleton : MonoBehaviour
         }
 
         #endif
-
-        location.text = "starting";
 
         UnityEngine.Input.location.Start(500f, 500f);
                 
@@ -87,17 +85,13 @@ public class GPSSingleton : MonoBehaviour
 
         // Service didn't initialize in 15 seconds
         if (maxWait < 1) {
-            // TODO Failure
             Debug.Log("Timed out");
             yield break;
         }
 
-        location.text = "checking connection";
-
         // Connection has failed
         if (UnityEngine.Input.location.status != LocationServiceStatus.Running) {
             Debug.LogFormat("Unable to determine device location. Failed with status {0}", UnityEngine.Input.location.status);
-            location.text = "unable to determine location";
             yield break;
         } else {
             Debug.LogFormat("Location service live. status {0}", UnityEngine.Input.location.status);
@@ -108,52 +102,45 @@ public class GPSSingleton : MonoBehaviour
                 + UnityEngine.Input.location.lastData.altitude + " " 
                 + UnityEngine.Input.location.lastData.horizontalAccuracy + " " 
                 + UnityEngine.Input.location.lastData.timestamp);
-
-            location.text = "Location: " 
-                + UnityEngine.Input.location.lastData.latitude + " " 
-                + UnityEngine.Input.location.lastData.longitude + " " 
-                + UnityEngine.Input.location.lastData.altitude + " " 
-                + UnityEngine.Input.location.lastData.horizontalAccuracy + " " 
-                + UnityEngine.Input.location.lastData.timestamp;
-
-            var _latitude = UnityEngine.Input.location.lastData.latitude;
-            var _longitude = UnityEngine.Input.location.lastData.longitude;
         }
 
-        int i = 0;
+        //update values until we become dissconnected
         while (UnityEngine.Input.location.status == LocationServiceStatus.Running) {
             this.latitude = UnityEngine.Input.location.lastData.latitude;
             this.longitude = UnityEngine.Input.location.lastData.longitude;
             this.lastUpdate = UnityEngine.Input.location.lastData.timestamp;
 
-            location.text = "Location: " 
-                + i + " : " 
-                + UnityEngine.Input.location.lastData.timestamp;
+            Debug.Log("Location: " 
+                + UnityEngine.Input.location.lastData.latitude + " " 
+                + UnityEngine.Input.location.lastData.longitude + " " 
+                + UnityEngine.Input.location.lastData.timestamp);
 
             yield return new WaitForSeconds(1f);
-            i++;
         }
 
-        // Stop service if there is no need to query location updates continuously
+        // Stop service when done
         UnityEngine.Input.location.Stop();
-        location.text = "stop";
     }
 
+    //returns true if the singleton has valid GPS coordinates
     public bool isDataValid()
     {
         return this.lastUpdate != -1;
     }
 
+    //returns the timestamp of the last coordinates update
     public double getLastUpdate()
     {
         return this.lastUpdate;
     }
 
+    //returns the latitude in decimal degrees
     public double getLatitude()
     {
         return this.latitude;
     }
 
+    //returns the longitude in decimal degrees
     public double getLongitude()
     {
         return this.longitude;
@@ -162,7 +149,6 @@ public class GPSSingleton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        location.text = "start function";
         StartCoroutine(GPSCoroutine());
     }
 
