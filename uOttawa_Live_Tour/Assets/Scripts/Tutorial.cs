ecba929d4bd1bde73c6198d99eeb784517bbd92b
Alan Harris <aharr033@uottawa.ca>
2022-11-03 14:43:16 -0400

    added final script/audio, tutorial scene

---
+++ uOttawa_Live_Tour/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject[] textScreens = new GameObject[5];
+    public Text nextButtonText;
+    private int screenIndex = 0;    
+    public GameObject camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textScreens[screenIndex].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void next(){
+        if (screenIndex == textScreens.Length -1){
+            camera.GetComponent<SceneSwitch>().loadTour();
+        }
+        if (screenIndex < textScreens.Length -1){
+            textScreens[screenIndex].SetActive(false);
+            screenIndex++;
+            textScreens[screenIndex].SetActive(true);
+        }
+        if (screenIndex == textScreens.Length -1){
+            nextButtonText.text = "Finish";
+        }
+    }
+
+    public void previous(){
+        if (screenIndex == textScreens.Length -1){
+            nextButtonText.text = "Next";
+        }
+        if (screenIndex > 0){
+            textScreens[screenIndex].SetActive(false);
+            screenIndex--;
+            textScreens[screenIndex].SetActive(true);
+        }
+        
+    }
+}
