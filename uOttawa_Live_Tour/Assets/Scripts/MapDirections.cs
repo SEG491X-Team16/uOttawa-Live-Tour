b92fb8b7ce6163f813e3a16e2e3ccbf9c0fa71d7
Alan Harris <aharr033@uottawa.ca>
2022-11-08 08:07:32 -0500

    edited map arrow

--- uOttawa_Live_Tour/Assets/Scripts/MapDirections.cs
+++ uOttawa_Live_Tour/Assets/Scripts/MapDirections.cs
@@ -26,21 +26,12 @@ public class MapDirections : MonoBehaviour
     private MoveMarker moveMarker;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         lineRenderer = GetComponent<LineRenderer>();
         map = GameObject.Find("uOttawaMap");
         moveMarker = this.GetComponent<MoveMarker>();
 
-        // Example directions
-        // GPSCoords c1 = new GPSCoords(45.423370f, -75.683817f); first point
-        // GPSCoords c2 = new GPSCoords(45.422591f, -75.682041f);
-        // GPSCoords c3 = new GPSCoords(45.422207f, -75.683533f); last point
-        // waypoints = new Waypoint[3];
-        // waypoints[0] = new Waypoint(c1,1);
-        // waypoints[1] = new Waypoint(c2,2);
-        // waypoints[2] = new Waypoint(c3,3);
-        // setDirections(waypoints);
+        TestPoints();
 
     }
 
@@ -70,19 +61,31 @@ public class MapDirections : MonoBehaviour
 
     //Updates the correct frame of the direction arrow's animation     
     //Array of GOs are transformed within map's localspace and their global positioning is used as endpoints for the linerenderer
-    void LateUpdate()
-    {
+    void LateUpdate() {
         counter += Time.deltaTime;
         if (counter >= 1f/fps){
             frame = ++frame % frames.Length;
             lineRenderer.material.SetTexture("_MainTex", frames[frame]);
             counter = 0f;
         }
-
-        for(int i = 0; i < waypoints.Length; i++){
-            go[i].transform.localPosition = new Vector3(moveMarker.GetLonToX(waypoints[i].Coordinates.Longitude), y , moveMarker.GetLatToZ(waypoints[i].Coordinates.Latitude));
-            lineRenderer.SetPosition(i, go[i].transform.position);
+        if (waypoints != null){
+            for(int i = 0; i < waypoints.Length; i++){
+                go[i].transform.localPosition = new Vector3(moveMarker.GetLonToX(waypoints[i].Coordinates.Longitude), y , moveMarker.GetLatToZ(waypoints[i].Coordinates.Latitude));
+                lineRenderer.SetPosition(i, go[i].transform.position);
+            }
         }
+    }
 
+    // Example directions
+    void TestPoints(){
+              
+        GPSCoords c1 = new GPSCoords(45.423370f, -75.683817f); //first point
+        GPSCoords c2 = new GPSCoords(45.422591f, -75.682041f);
+        GPSCoords c3 = new GPSCoords(45.422207f, -75.683533f); //last point
+        waypoints = new Waypoint[3];
+        waypoints[0] = new Waypoint(c1,1);
+        waypoints[1] = new Waypoint(c2,2);
+        waypoints[2] = new Waypoint(c3,3);
+        setDirections(waypoints);
     }
 }
